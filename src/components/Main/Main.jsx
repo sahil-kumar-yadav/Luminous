c38b6047a513996871a9253f26b383f46de69372
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2025-08-13 03:07:28 +0000

    rebuilding

--- src/components/Main/Main.jsx
+++
@@ -1,9 +0,0 @@
-const Main = ({ children }) => {
-    return (
-        <main className="bg-white dark:bg-gray-900" style={{ minHeight: 'calc(100vh - 145px)' }}>
-            {children}
-        </main>
-    );
-};
-
-export default Main;
--- src/components/Navbar/Icon.jsx
+++
@@ -1,9 +0,0 @@
-const Icon = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" role="button" tabindex="0" width="24" height="24" class="h-6 w-6 shrink-0" aria-label="bars 3" fill="none" viewBox="0 0 24 24" stroke-width="2">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
-        </svg>
-    );
-}
-
-export default Icon;
